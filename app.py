24755387ce020a7c90f946d56beacfaac0df08a0
Minh Vương <139727818+MinhVuong265@users.noreply.github.com>
2024-10-08 09:57:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -305,5 +305,5 @@ def predict():
         'bagging': bagging_pred.round(2)
     })
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000)) 
+    port = int(os.environ.get('PORT', 4000)) 
     app.run(debug=True, host='0.0.0.0', port=port)
